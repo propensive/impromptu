5b4bb8d9c48bee091cb09948170036fa59582491
Jon Pretty <jon.pretty@propensive.com>
2017-07-06 14:42:09 +0200

    Fully commented actor.scala

--- core/src/main/scala/impromptu/actor.scala
+++ core/src/main/scala/impromptu/actor.scala
@@ -18,19 +18,70 @@ import scala.util.Try
 import scala.concurrent._
 import language.implicitConversions
 import scala.reflect.runtime.universe.TypeTag
-import annotation.unchecked.{uncheckedVariance => uv}
+import scala.annotation.unchecked.{uncheckedVariance => uv}
+import scala.annotation.implicitNotFound
 
+/** the `impromptu` package object */
+object `package` {
+  /** convenience method for constructing a new [[Handler]]
+    * 
+    * @param cases  the cases to be handled
+    * @tparam Accept  the intersection type of the message types to be handled
+    * @tparam State   the least-upper-bound return type of all the cases
+    * @return a new [[Handler]]
+    */
+  def handle[Accept, State](cases: Actor.Case[Accept, State]*): Actor.Handler[Accept, State] =
+    Actor.Handler(cases.map { c => c.index -> c.fn }.toMap)
+ 
+  /** constructs an intermediate [[Apply]] factory object for defining [[Case]]s
+    *
+    * @tparam Msg  the message type to handle
+    * @return a new [[Apply]] instance for constructing a new [[Case]]
+    */
+  def on[Msg] = Apply[Msg]()
+  
+  /** intermediate factory object for constructing new [[Case]]s for a particular message type
+    *
+    * @tparam Msg  the fixed message type for which to construct a new [[Case]]
+    */
+  case class Apply[Msg] private () {
+    def apply[State](action: Msg => State)(implicit tag: TypeTag[Msg]): Actor.Case[Msg, State] =
+      Actor.Case(Actor.TypeIndex[Msg](tag), action)
+  }
+}
+
+/** factory for creating [[Actor]]s */
 object Actor {
+  /** creates a new [[Actor]]
+    * 
+    * @param init     the initial value of the [[Actor]]'s state
+    * @param action   the action which creates the [[Handler]] which will process messages
+    * @param execCtx  the [[ExecutionContext]] on which to execute the action
+    * @tparam Accept  the intersection type of all the types the actor can accept
+    * @tparam State   the type of the [[Actor]]'s state
+    * @return a new [[Actor]], ready to start receiving messages
+    */
   def apply[Accept, State](init: State)(action: State => Handler[Accept, State])(implicit
                            execCtx: ExecutionContext): Actor[State, Accept] =
     new Actor[State, Accept](init, action)
   
-  case class Handler[-Accept, +State](fns: Map[TypeIndex[_], Accept => State]) {
+  /** represents the functions which operate on a number of different message types
+    *
+    * @param fns  the functions 
+    * @tparam Accept  the intersection type of all the message types the [[Actor]] can accept
+    * @tparam State   the type of the [[Actor]]'s state
+    */
+  final case class Handler[-Accept, +State] private (fns: Map[TypeIndex[_], Accept => State]) {
     def handle[Msg: TypeTag](msg: Msg): State =
       fns(TypeIndex(implicitly[TypeTag[Msg]])).asInstanceOf[Msg => State](msg)
   }
 
-  case class TypeIndex[T](tag: TypeTag[T]) {
+  /** an index for [[TypeTag]]s which allows them to be looked up in a [[Map]]
+    *
+    * @tparam T  the type being indexed
+    * @param tag  the [[TypeTag]] for the type to be indexed
+    */
+  final case class TypeIndex[T] private (tag: TypeTag[T]) {
     override def equals(that: Any): Boolean = that match {
       case that: TypeIndex[_] => tag.tpe =:= that.tag.tpe
       case _ => false
@@ -38,29 +89,43 @@ object Actor {
     override def hashCode: Int = tag.tpe.hashCode
   }
 
-  case class Case[-Type, +State](index: TypeIndex[Type @uv], fn: Type => State)
+  /** one case, corresponding to one type, to be matched in a [[Handler]]
+    *
+    * @tparam Msg    the type of message being handled by this [[Case]]
+    * @tparam State  the return type from this case, corresponding to the type of the enclosing
+    *                [[Actor]]'s state
+    */
+  case class Case[-Msg, +State] private (index: TypeIndex[Msg @uv], fn: Msg => State)
 }
 
-object handle {
-  def apply[Accept, State](cases: Actor.Case[Accept, State]*): Actor.Handler[Accept, State] =
-    Actor.Handler(cases.map { c => c.index -> c.fn }.toMap)
-}
-
+/** an actor
+  *
+  * @param state    the state of the actor
+  * @param handler  constructs a new handler based on the actor's state
+  * @param execCtx  the execution context in which computations should be done
+  * @tparam State   the type of the actor's state
+  * @tparam Accept  the intersection type of the messages the actor can receive
+  */
 class Actor[State, Accept] private (val state: State,
                                     val handler: State => Actor.Handler[Accept, State])(implicit
                                     execCtx: ExecutionContext) {
   private[this] var currentFuture = Future(state)
-  def send[Msg: TypeTag](msg: Msg)(implicit ev: Accept <:< Msg): Unit = synchronized {
+
+  /** type alias for providing more appropriate error messages */
+  @implicitNotFound("this actor cannot accept messages of type ${Msg}")
+  type In[Msg, Accept] = Accept <:< Msg
+
+  /** sends a message to this actor
+    *
+    * @param msg  the message to send
+    * @param ev   evidence that the actor can receive messages of this type
+    * @tparam Msg  the type of the message to send
+    * @return a unit
+    */
+  def send[Msg: TypeTag](msg: Msg)(implicit ev: Msg In Accept): Unit = synchronized {
     currentFuture = currentFuture.flatMap { oldState =>
       Future(handler(oldState).handle(msg))
     }
   }
 }
 
-object on {
-  def apply[Type] = Apply[Type]()
-  case class Apply[Type]() {
-    def apply[State](action: Type => State)(implicit tag: TypeTag[Type]): Actor.Case[Type, State] =
-      Actor.Case(Actor.TypeIndex[Type](tag), action)
-  }
-}
