e23d9adfb5b7bc6b0e4802961ff12955faba423f
Jon Pretty <jon.pretty@propensive.com>
2017-07-07 01:31:32 +0200

    More cleanup

--- core/src/main/scala/impromptu/impromptu.scala
+++ core/src/main/scala/impromptu/impromptu.scala
@@ -20,22 +20,23 @@ import scala.language.implicitConversions
 
 /** factory object for [[Async]] instances */
 object Async {
+
   /** creates a new [[Async]] instance */
   def apply[Return](action: => Return)(implicit execCtx: ExecutionContext): Async[Return, _] =
     new Async[Return, Nothing](Seq(), env => action)
   
   /** constructs a new asynchronous computation which should run after the specified prerequisites
     * 
-    * @param deps     the prerequisite dependencies of the computation
-    * @param action   the computation to run
-    * @param execCtx  the execution context in which to run the computation
+    * @param dependencies  the prerequisite dependencies of the computation
+    * @param action        the computation to run
+    * @param execCtx       the execution context in which to run the computation
     * @tparam Before  the intersection type of the singleton types of the dependencies
     * @tparam Return  the type of the result of the computation
     * @return a newly-constructed, unevaluated [[Async]] instance
     */
-  def after[Before <: Async[_, _], Return](deps: Dependency[Before]*)(
+  def after[Before <: Async[_, _], Return](dependencies: Dependency[Before]*)(
       action: Env[Before] => Return)(implicit execCtx: ExecutionContext): Async[Return, Before] =
-    new Async[Return, Before](deps.map(_.async), action)
+    new Async[Return, Before](dependencies.map(_.async), action)
 
   /** automatically wraps any [[Async]] values in the contravariant [[Dependency]] type to ensure
     * that their type parameters infer to the intersection type
@@ -63,13 +64,13 @@ object Async {
 
 /** an asynchronously computed value, much like a lazy [[Future]]
   * 
-  * @param deps    the dependencies which must be computer prior to this [[Async]] value
-  * @param action  the action which computes the value
+  * @param dependencies  the dependencies which must be computer prior to this [[Async]] value
+  * @param action        the action which computes the value
   * @tparam Return  the type of the value to be computed
   * @tparam Before  an intersection type of the singleton types of all the dependencies of this
   *                 [[Async]] value
   */
-class Async[+Return, Before] private (val deps: Seq[Async[_, _]],
+final class Async[+Return, Before] private (val dependencies: Seq[Async[_, _]],
     val action: Async.Env[Before] => Return)(implicit execCtx: ExecutionContext) {
   
   /** returns the precomputed result of the previous dependent value, without blocking
@@ -78,12 +79,11 @@ class Async[+Return, Before] private (val deps: Seq[Async[_, _]],
     *             accessed without blocking
     * @return the precomputed value from the earlier asynchronous computation
     */
-  def apply()(implicit env: Async.Env[this.type]): Return =
-    future.value.get.get.asInstanceOf[Return]
+  def apply()(implicit env: Async.Env[this.type]): Return = future.value.get.get
 
   /** the lazily-evaluated [[Future]] corresponding to this asynchronous value */
   lazy val future: Future[Return] =
-    Future.sequence(deps.map(_.future)).map { _ => action(Async.AnyEnv) }
+    Future.sequence(dependencies.map(_.future)).map { _ => action(Async.AnyEnv) }
 
   /** returns the result from evaluating the future, blocking if necessary
     *
@@ -91,4 +91,3 @@ class Async[+Return, Before] private (val deps: Seq[Async[_, _]],
     */
   def await(): Return = Await.result(future, duration.Duration.Inf)
 }
-
